6c695fc45bc94f7b364692d659680213da431d16
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-05-24 16:57:34 +0100

    Initial commit

---
+++ src/main/java/com/ifeanyi/netflixmovies/NetflixMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.ifeanyi.netflixmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifeanyi/netflixmovies/NetflixMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifeanyi.netflixmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
